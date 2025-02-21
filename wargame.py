91d97b74a902de40a232e16e63f8724294e664f4
Nui <wanuhi.davtyan@gmail.com>
2025-02-21 12:35:10 +0100

    posh-vikings v6

--- wargame.py
+++ wargame.py
@@ -9,25 +9,26 @@ import pygame
 soldier_names = ["albert","andres","archie","dani", "david","gerard","german","graham","imanol","laura"]
 great_war = War()
 
-#Create 5 Vikings
-for i in range(0,5):
-    if i:
-        great_war.addViking(Viking(soldier_names[random.randint(0,9)],100,random.randint(0,100)))
+def main():
+    #Create 5 Vikings
+    for i in range(0,5):
+        if i:
+            great_war.addViking(Viking(soldier_names[random.randint(0,9)],100,random.randint(0,100)))
 
-#Create 5 Saxons
-for i in range(0,5):
-    if i:
-        great_war.addSaxon(Saxon(100,random.randint(0,100)))
-    
-round = 0
-while great_war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
-    great_war.vikingAttack()
-    print(great_war.vikingAttack())
+    #Create 5 Saxons
+    for i in range(0,5):
+        if i:
+            great_war.addSaxon(Saxon(100,random.randint(0,100)))
+        
+    round = 0
+    while great_war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
+        great_war.vikingAttack()
+        print(great_war.vikingAttack())
 
-    great_war.saxonAttack()
-    print(great_war.saxonAttack())
+        great_war.saxonAttack()
+        print(great_war.saxonAttack())
 
 
-    print(f"round: {round} // Viking army: {len(great_war.vikingArmy)} warriors",f"and Saxon army: {len(great_war.saxonArmy)} warriors")
-    print(great_war.showStatus())
-    round += 1
+        print(f"round: {round} // Viking army: {len(great_war.vikingArmy)} warriors",f"and Saxon army: {len(great_war.saxonArmy)} warriors")
+        print(great_war.showStatus())
+        round += 1
