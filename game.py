acf8fef86258318b9b229578b06c7e7b1dd9c661
EmiliaHorton12 <emiliavictoriah@gmail.com>
2025-02-21 10:58:07 +0100

    background

--- game.py
+++ game.py
@@ -1,14 +1,59 @@
+# import pygame
+# import sys
+
+# pygame.init()
+
+# #game window dimension
+# width = 800
+# height = 600
+
+# window = pygame.display.set_mode((width, height))
+# pygame.display.set_caption("Vikings VS Saxons")
+
+# frames = pygame.time.Clock()
+
 import pygame
-import sys
 
+# pygame setup
 pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
 
-#game window dimension
-width = 800
-height = 600
+background = pygame.image.load("Background.png").convert()
+background = pygame.transform.scale(background, (1280, 720))
 
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Vikings VS Saxons")
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-frames = pygame.time.Clock()
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    # fill the screen with a color to wipe away anything from last frame
+    screen.blit(background, (0, 0))
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
